1b42cc115c071c6065d84859ae1bdae6a1adbced
NeginSal <neginsaljooghi@gmail.com>
2025-04-20 16:15:42 +0330

    added router

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 var users = []User{
 	{ID: 1, Name: "mario", Email: "mario@gmail.com"},
@@ -8,5 +12,13 @@ var users = []User{
 }
 
 func main() {
-	fmt.Println("hello,world")
+	router := gin.Default()
+
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getUserByID)
+	router.POST("/users", createUser)
+	router.PUT("/users/:id", updateUser)
+	router.DELETE("/users/:id", deleteUser)
+
+	router.Run("localhost:8080")
 }
