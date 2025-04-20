c0c0d380270ce2c358a25cbf0e8dbba7d47a5984
NeginSal <neginsaljooghi@gmail.com>
2025-04-20 16:35:33 +0330

    add get /users

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-
+	"crud-api-go/models"
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
-var users = []User{
+var users = []models.User{
 	{ID: 1, Name: "mario", Email: "mario@gmail.com"},
 	{ID: 2, Name: "luigi", Email: "luigi@gmail.com"},
 }
@@ -15,10 +15,11 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/users", getUsers)
-	router.GET("/users/:id", getUserByID)
-	router.POST("/users", createUser)
-	router.PUT("/users/:id", updateUser)
-	router.DELETE("/users/:id", deleteUser)
 
 	router.Run("localhost:8080")
+}
+
+
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
 }
