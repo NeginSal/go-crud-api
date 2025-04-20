24dec6538aaf74925997b0fe9fd75511b11278bc
NeginSal <neginsaljooghi@gmail.com>
2025-04-20 16:07:45 +0330

    add models.go & users

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var users = []User{
+	{ID: 1, Name: "mario", Email: "mario@gmail.com"},
+	{ID: 2, Name: "luigi", Email: "luigi@gmail.com"},
+}
+
 func main() {
 	fmt.Println("hello,world")
 }
