a9fa3e242014645d38b0d4d42eb1b15022c44424
NeginSal <neginsaljooghi@gmail.com>
2025-04-21 16:43:33 +0330

    feat:Added getUserByID

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"crud-api-go/models"
 	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,15 +17,27 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/users", getUsers)
-	router.GET("/users/:id",getUserByID)
-	router.POST("/users",createUser)
-	router.PUT("/users/:id",updateUser)
-	router.DELETE("/users/:id",deleteUser)
+	router.GET("/users/:id", getUserByID)
+	router.POST("/users", createUser)
+	router.PUT("/users/:id", updateUser)
+	router.DELETE("/users/:id", deleteUser)
 
 	router.Run("localhost:8080")
 }
 
-
 func getUsers(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, users)
+}
+
+func getUserByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, user := range users {
+		if id == strconv.Itoa(user.ID) {
+			c.IndentedJSON(http.StatusOK, user)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found."})
 }
