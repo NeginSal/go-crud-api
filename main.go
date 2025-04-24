5ad723d2b89fc7b5ec759ed71289b9b4531197c2
NeginSal <neginsaljooghi@gmail.com>
2025-04-24 12:36:46 +0330

    added config file for db

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"crud-api-go/config"
 	"crud-api-go/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	config.ConnectDB()
+
 	router := gin.Default()
 
 	routes.UserRoutes(router)
 
 	router.Run("localhost:8080")
+
 }
